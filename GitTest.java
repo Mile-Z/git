1f0bcb1e6dfd63542845e663b1ed3daeacaf70ce
mile <codingMonkeyMile@qq.com>
2021-09-27 08:24:29 +0800

    fixup! hot-fix commit

--- GitTest.java
+++ GitTest.java
@@ -5,6 +5,5 @@ public class GitTest {
         System.out.println("Hello World!");
         System.out.println("Hello git!");
         System.out.println("Hello git2!");
-        System.out.println("Hello git3!");
     }
 }
